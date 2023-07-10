69bbecf7a11e0c510b7b16bbe1a66a61f0d6f7d1
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-07-10 23:53:04 +0600

    404 pages added

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
 import notfound from "../../Images/404 Not Found.jpg";
 
 const NotFound = () => {
   return (
     <div>
+      <div className="text-center my-3">
+        <Link to={"/"}>
+          <button className="btn btn-info">Back to Home</button>
+        </Link>
+      </div>
       <img src={notfound} alt="" />
     </div>
   );
